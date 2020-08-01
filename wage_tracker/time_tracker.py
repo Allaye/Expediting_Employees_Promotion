6a2c7c0f4710e1412ad107e1d8462e8aa72fff42
Allaye <kolade199@gmail.com>
2020-08-01 22:10:43 +0000

    add the data function

--- wage_tracker/time_tracker.py
+++ wage_tracker/time_tracker.py
@@ -33,13 +33,51 @@ def time_tracker(starttime, endtime, filename, extension, wageperhour = 5):
 
 
 
+def save_time_tracker_details( eyear, emon, eday, ehour, emin, syear, smon, sday, shour,
+                              smin , wageperhour, totalwage, totaltime, filename, extension):
+    
+    """
+    @save_time_tracker_details is a simple function that just saves the data generated form the time_tracker fuction 
+    into a csv or excel file,  
+    """
+    dataname = "".join([filename, extension])     #joining the filename and the extension
+    col = np.array(['eyear', 'emon', 'eday', 'ehour', 'emin', 'syear', 'smon', 'sday', 'shour',
+                              'smin' , 'wageperhour', 'totalwage', 'time_worked'])    #creating an array
+    tracker_data = pd.DataFrame(data=[[eyear, emon, eday, ehour, emin,syear, smon, sday, shour,
+                              smin , wageperhour, totalwage, totaltime]], columns=col)     #creating the dataframe 
+    
+    if extension == '.csv' or '.CSV':      #saving the dataframe as a csv or excel
 
+        tracker_data.to_csv(dataname)    
+    else:
+        tracker_data.to_excel(dataname)
+
+
+
+def save_time_tracker_details( eyear, emon, eday, ehour, emin, syear, smon, sday, shour,
+                              smin , wageperhour, totalwage, totaltime, filename, extension):
+    
+    """
+    @save_time_tracker_details is a simple function that just saves the data generated form the time_tracker fuction 
+    into a csv or excel file,  
+    """
+    dataname = "".join([filename, extension])     #joining the filename and the extension
+    col = np.array(['eyear', 'emon', 'eday', 'ehour', 'emin', 'syear', 'smon', 'sday', 'shour',
+                              'smin' , 'wageperhour', 'totalwage', 'time_worked'])    #creating an array
+    tracker_data = pd.DataFrame(data=[[eyear, emon, eday, ehour, emin,syear, smon, sday, shour,
+                              smin , wageperhour, totalwage, totaltime]], columns=col)     #creating the dataframe 
+    
+    if extension == '.csv' or '.CSV':      #saving the dataframe as a csv or excel
+
+        tracker_data.to_csv(dataname)    
+    else:
+        tracker_data.to_excel(dataname)
 
 
 
 def main():
     '''
-    @Evans if you still want to use the user input in the code add it here, if you are done delete this comment
+    @Evans if you still want to use the user input in the code add it here, if you are done delete this commet
     '''
     start = 2020,7,30,8,20
     end =  2020,7,31,12,30
